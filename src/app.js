097dbb1d1588f0ccf21df721da504df24fe0dc30
orkunbahcivann <orkunbahcivan@gmail.com>
2021-10-19 00:22:18 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+dolarDun= "9.20"
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+// euroDun = 11 // bu hatalı
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi","Emlak Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+console.log("<ul>")
+for (let i =0;i<konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>Konut Kredisi</li>
+    <li>Emlak Kredisi</li>
+    <li>Kamu Konut Kredisi</li>  
+</ul> */}
+console.log(konutKredileri)
